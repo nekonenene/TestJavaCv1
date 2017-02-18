dfd8cb5032727f3e6ea594554ba22aaf15b61ccb
ハトネコエ <hato.nekoe@gmail.com>
2017-02-19 02:40:37 +0900

    赤色画像作成メソッドをクラスに入れた

--- src/main/kotlin/hatonekoe/ImageCreator.kt
+++ src/main/kotlin/hatonekoe/ImageCreator.kt
@@ -3,12 +3,12 @@ package hatonekoe
 import org.bytedeco.javacpp.opencv_core.*
 import org.bytedeco.javacpp.opencv_imgcodecs.imwrite
 
-class CreateImage {
+class ImageCreator {
     fun createRedImg() {
         try {
             println("赤色の画像を出力します")
 
-            val redImage: Mat = Mat(100, 200, CV_8UC3, Scalar(0.0, 0.0, 255.0, 0.0))
+            val redImage = Mat(100, 200, CV_8UC3, Scalar(0.0, 0.0, 255.0, 0.0))
             imwrite("../redImg.jpg", redImage)
         } catch (e: Exception) {
             println(e)
@@ -17,5 +17,6 @@ class CreateImage {
 }
 
 fun main(args: Array<String>) {
-    CreateImage.cre
+    val creator = ImageCreator()
+    creator.createRedImg()
 }
